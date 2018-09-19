aedf370206b87eb3867e8319c8b97bd7c258961e
Damian Ubowski <sverda300@gmail.com>
2018-09-19 22:43:13 +0200

    Add project files.

---
+++ PokeQuiz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PokeQuiz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
