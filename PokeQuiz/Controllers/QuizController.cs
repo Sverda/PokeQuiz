2630466ec56b7e58da3254c85af6758ae3776f2b
Damian Ubowski <sverda300@gmail.com>
2018-10-09 20:05:58 +0200

    QuizView: Checking answer for question

--- PokeQuiz/Controllers/QuizController.cs
+++ PokeQuiz/Controllers/QuizController.cs
@@ -5,26 +5,35 @@ using PokeQuiz.Services.Interfaces;
 
 namespace PokeQuiz.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/quiz")]
     public class QuizController : ControllerBase
     {
         private readonly IPokemonService _pokemonService;
+        private QuizViewModel _questionData;
 
         public QuizController(IPokemonService pokemonService)
         {
             _pokemonService = pokemonService;
         }
 
-        public QuizViewModel Get()
+        [Route("question")]
+        public QuizViewModel GetQuestion()
         {
             var correctAnswer = _pokemonService.ChooseRandom();
             var fakeAnswers = Enumerable.Range(1, 3).Select(_ => _pokemonService.ChooseRandom().Name).ToList();
             fakeAnswers.Add(correctAnswer.Name);
-            return new QuizViewModel
+            _questionData = new QuizViewModel
             {
                 CorrectAnswer = correctAnswer,
                 FakeAnswers = fakeAnswers
             };
+            return _questionData;
+        }
+
+        [Route("answer/{name}")]
+        public bool GetAnswer(string name)
+        {
+            return name == _questionData?.CorrectAnswer.Name;
         }
     }
 }
